ec48f1151ff892dfdae76d3ea438361711e20854
Peczyn <marcinpieczonka03@gmail.com>
2023-12-02 21:48:55 +0100

    V2

--- main.cpp
+++ main.cpp
@@ -4,61 +4,180 @@
 #include <stack>
 using namespace std;
 
+
 class Maze{
 public:
-    vector<vector<char>> matrix {
-            {'1','1','1','1','1','1','0','1','1','0'},
-            {'1','1','1','1','0','1','0','0','0','0'},
-            {'1','1','1','1','0','0','1','1','1','1'},
-            {'1','1','1','1','0','0','1','0','0','1'},
-            {'1','1','1','1','0','0','1','0','0','1'},
-            {'1','1','1','1','0','0','1','0','0','1'},
-            {'1','0','0','1','1','0','1','0','0','1'},
-            {'1','0','0','1','1','1','1','1','0','1'},
-            {'1','0','1','1','1','0','1','0','0','1'},
-            {'1','1','1','1','1','0','1','0','0','1'},
-    };
+    int mWidth;
+    int mHeight;
+
+    int mVisited;
+    int mSize;
+    vector<vector<int>> mMaze;
+
     int startingPositionX = 1;
     int startingPositionY = 1;
-    int destinationX = 6;
-    int destinationY = 6;
+    int destinationX = 13;
+    int destinationY = 13;
+
+    vector<vector<char>> matrix;
+
+private:
+    enum{
+        North = 0x01,
+        East = 0x02,
+        South = 0x04,
+        West = 0x08,
+        CellVisited = 0x10,
+    }; //WSEN
+
+
+    stack<pair<int, int>> mStack;
+    int mPathWidth;
+
+public:
+    virtual bool CreateMaze(int width, int height);
+
+    virtual bool Update()
+    {
+        auto offsetX = [&](int x)
+        {
+            return (mStack.top().first + x);
+        };
+        auto offsetY = [&](int y)
+        {
+            return (mStack.top().second + y);
+        };
+
+        if(mVisited < mWidth*mHeight)
+        {
+            vector<int> neighbours;
+
+            if(mStack.top().second > 0 && (mMaze[offsetY(-1)][offsetX(0)] & CellVisited) ==0)
+            {
+                neighbours.push_back(0);
+            }
+            if(mStack.top().first < mWidth-1  && (mMaze[offsetY(0)][offsetX(1)] & CellVisited) ==0)
+            {
+                neighbours.push_back(1);
+            }
+            if(mStack.top().second < mHeight-1  && (mMaze[offsetY(1)][offsetX(0)] & CellVisited) ==0)
+            {
+                neighbours.push_back(2);
+            }
+            if(mStack.top().first > 0 && (mMaze[offsetY(0)][offsetX(-1)] & CellVisited) ==0)
+            {
+                neighbours.push_back(3);
+            }
+
+
+            if(!neighbours.empty())
+            {
+                int nextCellDir = neighbours[rand() % neighbours.size()];
+
+                switch(nextCellDir)
+                {
+                    case 0:
+                        mMaze[offsetY(-1)][offsetX(0)] |= CellVisited | South;
+                        mMaze[offsetY(0)][offsetX(0)]  |= North;
+                        mStack.push(make_pair((mStack.top().first+0),(mStack.top().second-1)));
+                        break;
+                    case 1:
+                        mMaze[offsetY(0)][offsetX(1)] |= CellVisited | West;
+                        mMaze[offsetY(0)][offsetX(0)]  |= East;
+                        mStack.push(make_pair((mStack.top().first+1),(mStack.top().second+0)));
+                        break;
+                    case 2:
+                        mMaze[offsetY(1)][offsetX(0)] |= CellVisited | North;
+                        mMaze[offsetY(0)][offsetX(0)]  |= South;
+                        mStack.push(make_pair((mStack.top().first+0),(mStack.top().second+1)));
+                        break;
+                    case 3:
+                        mMaze[offsetY(0)][offsetX(-1)] |= CellVisited | East;
+                        mMaze[offsetY(0)][offsetX(0)]  |= West;
+                        mStack.push(make_pair((mStack.top().first-1),(mStack.top().second+0)));
+                        break;
+                }
+                mVisited++;
+            }
+            else
+            {
+                mStack.pop();
+            }
+        }
+        return true;
+    }
 };
+
+
+
+template<typename T>
+void PrintMatrix(const vector<vector<T>>& matrix);
 
 vector<vector<float>> MazeCounter(Maze maze);
 
-template<typename T>
-void WypiszTablice(const vector<vector<T>>& matrix);
-
-vector<vector<char>> RozwiazanieLabiryntu(stack<char> moves, Maze maze);
+vector<vector<char>> MazeSolver(stack<char> moves, Maze maze);
 
 stack<char> test(Maze maze, vector<vector<float>> x);
 
+void StworzLabirynt(Maze &M);
 
 int main() {
+
     Maze maze;
+    StworzLabirynt(maze);
+    maze.destinationX= maze.matrix[0].size()-2;
+    maze.destinationY= maze.matrix.size()-2;
     vector<vector<float>> odleglosci = MazeCounter(maze);
-    WypiszTablice(odleglosci);
-
     stack<char> moves = test(maze,odleglosci);
-    vector<vector<char>> way = RozwiazanieLabiryntu(moves,maze);
+    vector<vector<char>> way = MazeSolver(moves,maze);
 
-    WypiszTablice(way);
+    PrintMatrix(maze.matrix);
+//    PrintMatrix(odleglosci);
+    PrintMatrix(way);
     return 0;
 }
 
 
+bool Maze::CreateMaze(int width, int height) {
+    mWidth = width;
+    mHeight = height;
+    mSize = mWidth*mHeight;
+
+    mMaze =  vector<vector<int>>(mHeight,vector<int>(mWidth,0));
+
+    int x = rand()%mWidth;
+    int y = rand()%mHeight;
+
+    mStack.push(make_pair(x,y));
+    mMaze[y][x]= CellVisited;
+    mVisited=1;
+
+    return true;
+}
+
+template<typename T>
+void PrintMatrix(const vector<vector<T>>& matrix)
+{
+    for(auto &a : matrix)
+    {
+        for(auto &b: a) cout << b << ' ';
+        cout << endl;
+    }
+    cout << endl;
+}
 
 vector<vector<float>> MazeCounter(Maze maze)
 {
-    vector<vector<float>> result;
-    for(int i=0; i<maze.matrix.size(); i++)
+    vector<vector<float>> CountedMatrix;
+
+    for(int row=0; row<maze.matrix.size(); row++)
     {
         vector<float> temp;
-        for(int j=0; j<maze.matrix.size(); j++)
+        for(int collumn=0; collumn<maze.matrix[0].size(); collumn++)
         {
-            if(maze.matrix[i][j]=='1')
+            if(maze.matrix[row][collumn]!='@')
             {
-                temp.push_back(sqrt(pow(i-maze.destinationX,2)+pow(j-maze.destinationY,2)));
+                temp.push_back(sqrt(pow(row-maze.destinationX,2)+pow(collumn-maze.destinationY,2)));
             }
             else
             {
@@ -66,28 +185,17 @@ vector<vector<float>> MazeCounter(Maze maze)
             }
 
         }
-        result.push_back(temp);
+        CountedMatrix.push_back(temp);
     }
-    return result;
+    return CountedMatrix;
 }
 
-template<typename T>
-void WypiszTablice(const vector<vector<T>>& matrix)
+vector<vector<char>> MazeSolver(stack<char> moves,Maze maze)
 {
-    for(auto &a : matrix)
-    {
-        for(auto &b: a) cout << b << '\t';
-        cout << endl;
-    }
-
-}
-
-vector<vector<char>> RozwiazanieLabiryntu(stack<char> moves,Maze maze)
-{
-    vector<vector<char>> way(10,vector<char>(10,'.'));
+    vector<vector<char>> way(maze.matrix.size(),vector<char>(maze.matrix[0].size(),'.'));
     int a = maze.destinationX;
     int b = maze.destinationY;
-    cout << endl << endl;
+
     while(!moves.empty())
     {
         way[b][a] = 'X';
@@ -104,6 +212,7 @@ vector<vector<char>> RozwiazanieLabiryntu(stack<char> moves,Maze maze)
 stack<char> test(Maze maze, vector<vector<float>> x)
 {
     stack<char> moves;
+    moves.push('X');
     int counter = 1;
     while(maze.startingPositionX!=maze.destinationX || maze.startingPositionY!=maze.destinationY){
         x[maze.startingPositionY][maze.startingPositionX]=-2;
@@ -117,7 +226,7 @@ stack<char> test(Maze maze, vector<vector<float>> x)
         float min= INT32_MAX;
 
         counter = 0;
-        for(int i=0; i<3; i++)
+        for(int i=0; i<4; i++)
         {
             if(directions[i]>=0) counter++;
             if(directions[i]<min && directions[i]>=0) min=directions[i];
@@ -125,6 +234,8 @@ stack<char> test(Maze maze, vector<vector<float>> x)
 
         if(counter==0)
         {
+            if(moves.top()=='X') moves.pop();
+            if(moves.empty()) return moves;
             while(moves.top()!='X')
             {
                 if(moves.top()=='N') maze.startingPositionY++;
@@ -161,4 +272,45 @@ stack<char> test(Maze maze, vector<vector<float>> x)
         }
     }
     return moves;
+}
+
+void StworzLabirynt(Maze &M)
+{
+    srand((unsigned) time(NULL));
+    M.CreateMaze(15,110);
+    while(M.mVisited!=M.mSize)
+    {
+        M.Update();
+    }
+
+
+
+    for(int i=0; i<M.mHeight*3; i++)
+    {
+        vector<char> temp;
+        for(int j=0; j<M.mWidth*3; j++)
+        {
+            temp.push_back('@');
+        }
+        M.matrix.push_back(temp);
+    }
+
+    for(int row =0; row<M.mHeight; row++)
+    {
+        for(int collumn=0; collumn<M.mWidth; collumn++) {
+            M.matrix[row*3+1][collumn*3+1]=' ';
+            if ((M.mMaze[row][collumn] & 0x01) == 1) {
+                M.matrix[row*3][collumn*3 + 1] = ' ';
+            }
+            if ((M.mMaze[row][collumn] & 0x02) == 2) {
+                M.matrix[row*3 + 1][collumn*3 + 2] = ' ';
+            }
+            if ((M.mMaze[row][collumn] & 0x04) == 4) {
+                M.matrix[row*3 + 2][collumn*3 + 1] = ' ';
+            }
+            if ((M.mMaze[row][collumn] & 0x08) == 8) {
+                M.matrix[row*3 + 1][collumn*3] = ' ';
+            }
+        }
+    }
 }
